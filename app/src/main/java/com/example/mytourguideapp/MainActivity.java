d04459babc47f47ca4e69a603bebbbbb121c96b6
zhavei <zhavei.vei@gmail.com>
2021-10-17 22:12:19 +0700

    masih bingung

--- app/src/main/java/com/example/mytourguideapp/MainActivity.java
+++ app/src/main/java/com/example/mytourguideapp/MainActivity.java
@@ -8,24 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<DataModelGunung> gunungList = MockData.getGunungList();
+    private List<DataModelGunung> gunungListDisplay = new ArrayList<>();
+    private GunungListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CardView moveDetail = findViewById(R.id.detail_tv);
-        moveDetail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToDetailActivity();
-            }
-        });
+
+
     }
 
-    private void goToDetailActivity() {
-        Intent intent = new Intent(this.getApplicationContext(), DetailActivity.class);
-        startActivity(intent);
-    }
 }
