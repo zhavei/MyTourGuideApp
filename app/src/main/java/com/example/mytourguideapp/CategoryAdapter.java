b7728f5fa729f6c5bce50575083f78a63a8bfa78
zhavei <zhavei.vei@gmail.com>
2021-10-20 22:37:06 +0700

    bisa bisa

--- app/src/main/java/com/example/mytourguideapp/CategoryAdapter.java
+++ app/src/main/java/com/example/mytourguideapp/CategoryAdapter.java
@@ -1,12 +1,7 @@
 package com.example.mytourguideapp;
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 
@@ -22,12 +17,12 @@ public class CategoryAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position)
         {
-            case 0: return new NumbersFragment();
+            case 0: return new MountainFragment();
             case 1: return new ColorsFragment();
             case 2: return new FamiliyFragment();
             case 3: return new PhrasesFragment();
         }
-        return new NumbersFragment();
+        return new MountainFragment();
     }
 
     @Override
--- app/src/main/java/com/example/mytourguideapp/PhrasesFragment.java
+++ app/src/main/java/com/example/mytourguideapp/PhrasesFragment.java
@@ -7,53 +7,58 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PhrasesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class PhrasesFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public PhrasesFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PhrasesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PhrasesFragment newInstance(String param1, String param2) {
+        PhrasesFragment fragment = new PhrasesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.word_list_activity, container, false);
-
-        final ArrayList<WordDataModel> numbersArray = new ArrayList<WordDataModel>();
-
-        numbersArray.add(new WordDataModel("lutti", "satu", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("otiiko", "dua", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("tolookosu", "tiga", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("oyyisa", "empat", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("massokka", "lima", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("temmokka", "enam", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("kenekaku", "tujuh", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("kawinta", "delapan", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("wo’e", "sembilan", R.drawable.ic_launcher_background));
-        numbersArray.add(new WordDataModel("na’aacha", "sepuluh", R.drawable.ic_launcher_background));
-
-        // using simple recycle view
-        WordAdapater stringArrayAdapter = new WordAdapater(getActivity(), numbersArray, R.color.white);
-        ListView listView = (ListView) rootView.findViewById(R.id.word_list_activity);
-        listView.setAdapter(stringArrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                WordDataModel wordDataModel = numbersArray.get(position);
-
-                Toast.makeText(getActivity(), " finish play", Toast.LENGTH_SHORT).show();
-            }
-        });
-
         // Inflate the layout for this fragment
-        return rootView;
+        return inflater.inflate(R.layout.fragment_phrases, container, false);
     }
 }
