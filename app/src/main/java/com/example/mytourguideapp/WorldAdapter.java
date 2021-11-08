50cbdfe32b8812d25cd49e0214c6f0e59a932cf9
zhavei <zhavei.vei@gmail.com>
2021-11-08 22:23:11 +0700

    change on RecycleViewWorldAdapter

--- app/src/main/java/com/example/mytourguideapp/WorldAdapter.java
+++ app/src/main/java/com/example/mytourguideapp/WorldAdapter.java
@@ -17,6 +17,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import java.util.ArrayList;
 import java.util.List;
 
+//cureently not using this
 public class WorldAdapter extends ArrayAdapter<WorldDataModel> {
 
     public WorldAdapter(Context context, ArrayList<WorldDataModel> worldDataModels) {
